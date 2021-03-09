3b83e4f0b037784b953413b4a66c3cdf9406fe7a
Bitake <76730254+Bitake@users.noreply.github.com>
2021-03-09 15:53:33 +0800

    Update Bitake_php_callback_demo.php

--- Bitake_demo/Bitake_php_callback_demo.php
+++ Bitake_demo/Bitake_php_callback_demo.php
@@ -25,50 +25,9 @@ $str = var_export($data,TRUE);
 
 $order_num=$data['orderNo'];
 
-    $con = mysqli_connect('localhost','','','');
-    if (!$con)
-    {
-        die('Could not connect: ' . mysqli_error($con));
-    }
-    
-    mysqli_select_db($con,"con_order");
+   
+ //查询订单，比对数据，为客户入金
  
-    mysqli_set_charset($con, "utf8");
- 
-    $sql = "select id,NUMBER,LOGIN,USD,CNY,sign,order_num from con_order where order_num='$order_num'";
-    $result = mysqli_query($con,$sql);
- 
-    while($row = mysqli_fetch_array($result))
-    {
-        
-    $NUMBER=$row['NUMBER'];
-    $LOGIN=$row['LOGIN'];
-    $USD=$row['USD'];
-    $CNY=$row['CNY'];
-    $sersign=$row['sign'];
-    $ISPAY="1";
-        
-    }
- 
-    mysqli_close($con);
-
-
-
-
-$arrs = array('NUMBER' => $NUMBER, 'LOGIN' => $LOGIN, 'USD' => $USD, 'CNY' => $CNY, 'ISPAY' => $ISPAY); //生成回调数组
-
-$arrsjson = json_encode($arrs); //回调数组转换为JSON字符串
-$arrjm = base64_encode($arrsjson); //回调数组64位加密
-$json = json_encode(['data' => $arrjm, 'sign' => $sersign]); //数组+sign一起加密
-
-//追加写入
-file_put_contents(__DIR__."/log.txt", date('Y-m-d H:i:s'). $arrsjson.PHP_EOL, FILE_APPEND);
-
-//if($sign !== $_POST['sign'] || $_POST['status'] !== 'success') die('fail');
- 
-$result = _http_request('这里填自动入金地址',array('callback' => base64_encode($json)));
-
-file_put_contents(__DIR__."/log.txt", date('Y-m-d H:i:s'). $result .PHP_EOL, FILE_APPEND);
 
 //HTTP请求（支持HTTP/HTTPS，支持GET/POST）
 function _http_request($url, $data = null)
